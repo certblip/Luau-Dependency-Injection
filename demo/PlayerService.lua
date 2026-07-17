087b4ebf5d5f611d9147cded570ee83be4c41961
certblip <bludosvkie@gmail.com>
2026-07-17 03:43:22 -0700

    Create PlayerService.lua

---
+++ demo/PlayerService.lua
@@ -0,0 +1,56 @@
+--[[
+	PlayerService
+
+	Coordinates player-related operations.
+
+	Depends on both InventoryService and EconomyService.
+]]
+
+local Players = game:GetService("Players")
+
+local Lifetime = require(script.Parent.Parent.src.Types.Lifetime)
+
+return {
+
+	Name = "PlayerService",
+
+	Lifetime = Lifetime.Singleton,
+
+	Constructor = function(resolve)
+
+		local Inventory = resolve("InventoryService")
+		local Economy = resolve("EconomyService")
+
+		local self = {}
+
+		function self:Init()
+
+			print("[PlayerService] Initialized.")
+
+			Players.PlayerAdded:Connect(function(player)
+
+				Inventory:AddItem(player.UserId, "Starter Sword")
+
+				Economy:AddCoins(player.UserId, 100)
+
+			end)
+
+		end
+
+		function self:GetCoins(player)
+
+			return Economy:GetCoins(player.UserId)
+
+		end
+
+		function self:GetInventory(player)
+
+			return Inventory:GetInventory(player.UserId)
+
+		end
+
+		return self
+
+	end
+
+}
